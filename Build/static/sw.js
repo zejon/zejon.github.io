9cb10eb329c9404c05c6ce19050e947ff4e70cae
zejon <128334757+zejon@users.noreply.github.com>
2026-01-19 17:17:36 +0800

    Delete Build/static directory

--- Build/static/sw.js
+++
@@ -1,7 +0,0 @@
-// Set up registration
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./service-worker.js')
-             .then(function() { console.log('Service Worker Registered'); });
-}
